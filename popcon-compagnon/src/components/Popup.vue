cc0f47a7ba1887264a185ba5766a13a662833909
Tosi Émeric <emeric254@users.noreply.github.com>
2023-09-12 10:18:06 +0200

    Mobile zoom (#40)
    Co-authored-by: Emeric Tosi <emeric@3dtrust.fr>

--- popcon-compagnon/src/components/Popup.vue
+++ popcon-compagnon/src/components/Popup.vue
@@ -11,21 +11,34 @@
       }"
       class="absolute flex justify-center items-center bg-popup-mask"
     >
-      <div class="rounded-xl bg-white shadow-lg relative overflow-hidden">
-        <div class="bg-sky-50 flex items-center justify-between p-3">
-          <h3 class="text-popcon-blue font-bold pr-2">
+      <div class="rounded-xl bg-white sm:shadow-lg overflow-hidden md:grow-0">
+        <div
+          :class="[
+            scale > 2.0 ? 'px-2 pt-2 pb-1' : 'p-4',
+            'bg-sky-50 flex items-center justify-between',
+          ]"
+        >
+          <h3
+            :style="{ 'font-size': 24 / scale + 'px' }"
+            class="text-popcon-blue font-bold pr-1 sm:pr-2 md:pr-3"
+          >
             {{ popupStore.title }}
           </h3>
           <button
+            :style="{ 'font-size': 32 / scale + 'px' }"
             @click="hidePopup"
             title="Fermer"
-            class="w-5 h-5 pb-0.5 flex justify-center items-center border-2 border-solid border-popcon-orange rounded-full text-popcon-orange hover:text-popcon-green hover:border-popcon-green transition-colors duration-200"
+            class="w-2 h-1 sm:w-3 sm:h-2 md:w-5 md:h-5 md:pb-0.5 flex justify-center items-center text-popcon-orange hover:text-popcon-green hover:border-popcon-green transition-colors duration-200"
           >
             &times;
           </button>
         </div>
 
-        <p v-if="popupStore.body" class="text-gray-700 p-3">
+        <p
+          v-if="popupStore.body"
+          :style="{ 'font-size': 24 / scale + 'px' }"
+          :class="[scale > 3.0 ? 'px-2 py-1' : 'p-4', 'text-gray-700']"
+        >
           {{ popupStore.body }}
         </p>
       </div>
@@ -46,6 +59,7 @@ export default {
       left: 0,
       width: 0,
       height: 0,
+      scale: 1,
     };
   },
 
@@ -72,6 +86,9 @@ export default {
       this.left = visualViewport.pageLeft;
       this.width = visualViewport.width;
       this.height = visualViewport.height;
+
+      // resize texte
+      this.scale = visualViewport.scale;
     },
 
     positionEvent() {
